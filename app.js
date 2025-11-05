86040247a57f2df02f7fade47c1593a4add292cb
Piyush Bhardwaj <piyush1102kecbhardwaj@gmail.com>
2025-11-05 12:34:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+let saveEl = document.getElementById("save-el")
+
+
+let countEl = document.getElementById("count-el")
+let count = 0;
+console.log(saveEl)
+function increment(){
+     count += 1
+     countEl.textContent = count
+     console.log(count)
+}
+
+
+function saves(){
+
+
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+
+
+    countEl.textContent = 0
+    count = 0
+
+
+    console.log(count)
+
+
+}
+saves()
